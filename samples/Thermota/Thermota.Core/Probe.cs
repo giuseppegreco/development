841a24443c7382d86ccb07a0e015b9bed67fac4d
Giuseppe Greco <j3d@giuseppes-macbook-pro-1.home>
2019-04-09 20:30:02 +0200

    first commit

---
+++ samples/Thermota/Thermota.Core/Probe.cs
@@ -0,0 +1,87 @@
+﻿#region Header
+//+ <source name="Probe.cs" language="C#" begin="2-Jan-2012">
+//+ <author href="mailto:giuseppe.greco@agamura.com">Giuseppe Greco</author>
+//+ <copyright year="2012">
+//+ <holder web="http://www.agamura.com">Agamura, Inc.</holder>
+//+ </copyright>
+//+ <legalnotice>All rights reserved.</legalnotice>
+//+ </source>
+#endregion
+
+#region References
+using System;
+using System.Threading;
+#endregion
+
+namespace Thermota.Core
+{
+    /// <summary>
+    /// Implements a virtual probe for simulating temperature detections.
+    /// </summary>
+    public class Probe
+    {
+        #region Fields
+        private const int AbsoluteZero = -273;
+        private const int MaxTemperature = 100;
+        private const int DetectionInterval = 5000;
+        private Thread detector;
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Occurs when a temperature detection is performed.
+        /// </summary>
+        public event EventHandler<DetectTemperatureEventArgs> DetectTemperature;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Probe"/> class.
+        /// </summary>
+        public Probe() { }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Starts temperature detections.
+        /// </summary>
+        public void Start()
+        {
+            detector = new Thread(new ThreadStart(Detect));
+            detector.Start();
+            detector.IsBackground = true;
+        }
+
+        /// <summary>
+        /// Stops temperature detections.
+        /// </summary>
+        public void Stop()
+        {
+            detector.Join(0);
+        }
+
+        /// <summary>
+        /// Simulates temperature detections.
+        /// </summary>
+        private void Detect()
+        {
+            int temperature;
+            Random random = new Random();
+
+            while (true) {
+                try {
+                    temperature = random.Next(AbsoluteZero, MaxTemperature);
+                } catch (ArgumentOutOfRangeException) {
+                    temperature = MaxTemperature;
+                }
+
+                if (DetectTemperature != null) {
+                    DetectTemperature(this, new DetectTemperatureEventArgs(temperature));
+                }
+
+                Thread.Sleep(DetectionInterval);
+            }
+        }
+        #endregion
+    }
+}
---
+++ samples/Thermota/Thermota/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Thermota Console")]
+[assembly: AssemblyDescription("Thermota User Interface")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Default - Debug")]
+#else
+[assembly: AssemblyConfiguration("Default")]
+#endif
+[assembly: AssemblyCompany("Agamura, Inc.")]
+[assembly: AssemblyProduct("Thermota")]
+[assembly: AssemblyCopyright("Copyright © Agamura, Inc. 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("331e69d0-84b7-4a1c-ba4b-76238cf05a18")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0")]
